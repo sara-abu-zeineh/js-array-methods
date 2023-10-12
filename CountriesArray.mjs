970500f0be01b85b97c33cbafd0b8b4f79486ee7
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-12 09:17:17 +0300

    Added population and cities data to each country object with unique IDs.

--- CountriesArray.mjs
+++ CountriesArray.mjs
@@ -1,207 +1,513 @@
 export const countries = [
-    "Afghanistan",
-    "Albania",
-    "Algeria",
-    "Andorra",
-    "Angola",
-    "Anguilla",
-    "Antigua &amp; Barbuda",
-    "Argentina",
-    "Armenia",
-    "Aruba",
-    "Australia",
-    "Austria",
-    "Azerbaijan",
-    "Bahamas",
-    "Bahrain",
-    "Bangladesh",
-    "Barbados",
-    "Belarus",
-    "Belgium",
-    "Belize",
-    "Benin",
-    "Bermuda",
-    "Bhutan",
-    "Bolivia",
-    "Bosnia &amp; Herzegovina",
-    "Botswana",
-    "Brazil",
-    "British Virgin Islands",
-    "Brunei",
-    "Bulgaria",
-    "Burkina Faso",
-    "Burundi",
-    "Cambodia",
-    "Cameroon",
-    "Cape Verde",
-    "Cayman Islands",
-    "Chad",
-    "Chile",
-    "China",
-    "Colombia",
-    "Congo",
-    "Cook Islands",
-    "Costa Rica",
-    "Cote D Ivoire",
-    "Croatia",
-    "Cruise Ship",
-    "Cuba",
-    "Cyprus",
-    "Czech Republic",
-    "Denmark",
-    "Djibouti",
-    "Dominica",
-    "Dominican Republic",
-    "Ecuador",
-    "Egypt",
-    "El Salvador",
-    "Equatorial Guinea",
-    "Estonia",
-    "Ethiopia",
-    "Falkland Islands",
-    "Faroe Islands",
-    "Fiji",
-    "Finland",
-    "France",
-    "French Polynesia",
-    "French West Indies",
-    "Gabon",
-    "Gambia",
-    "Georgia",
-    "Germany",
-    "Ghana",
-    "Gibraltar",
-    "Greece",
-    "Greenland",
-    "Grenada",
-    "Guam",
-    "Guatemala",
-    "Guernsey",
-    "Guinea",
-    "Guinea Bissau",
-    "Guyana",
-    "Haiti",
-    "Honduras",
-    "Hong Kong",
-    "Hungary",
-    "Iceland",
-    "India",
-    "Indonesia",
-    "Iran",
-    "Iraq",
-    "Ireland",
-    "Isle of Man",
-    "Israel",
-    "Italy",
-    "Jamaica",
-    "Japan",
-    "Jersey",
-    "Jordan",
-    "Kazakhstan",
-    "Kenya",
-    "Kuwait",
-    "Kyrgyz Republic",
-    "Laos",
-    "Latvia",
-    "Lebanon",
-    "Lesotho",
-    "Liberia",
-    "Libya",
-    "Liechtenstein",
-    "Lithuania",
-    "Luxembourg",
-    "Macau",
-    "Macedonia",
-    "Madagascar",
-    "Malawi",
-    "Malaysia",
-    "Maldives",
-    "Mali",
-    "Malta",
-    "Mauritania",
-    "Mauritius",
-    "Mexico",
-    "Moldova",
-    "Monaco",
-    "Mongolia",
-    "Montenegro",
-    "Montserrat",
-    "Morocco",
-    "Mozambique",
-    "Namibia",
-    "Nepal",
-    "Netherlands",
-    "Netherlands Antilles",
-    "New Caledonia",
-    "New Zealand",
-    "Nicaragua",
-    "Niger",
-    "Nigeria",
-    "Norway",
-    "Oman",
-    "Pakistan",
-    "Palestine",
-    "Panama",
-    "Papua New Guinea",
-    "Paraguay",
-    "Peru",
-    "Philippines",
-    "Poland",
-    "Portugal",
-    "Puerto Rico",
-    "Qatar",
-    "Reunion",
-    "Romania",
-    "Russia",
-    "Rwanda",
-    "Saint Pierre &amp; Miquelon",
-    "Samoa",
-    "San Marino",
-    "Satellite",
-    "Saudi Arabia",
-    "Senegal",
-    "Serbia",
-    "Seychelles",
-    "Sierra Leone",
-    "Singapore",
-    "Slovakia",
-    "Slovenia",
-    "South Africa",
-    "South Korea",
-    "Spain",
-    "Sri Lanka",
-    "St Kitts &amp; Nevis",
-    "St Lucia",
-    "St Vincent",
-    "St. Lucia",
-    "Sudan",
-    "Suriname",
-    "Swaziland",
-    "Sweden",
-    "Switzerland",
-    "Syria",
-    "Taiwan",
-    "Tajikistan",
-    "Tanzania",
-    "Thailand",
-    "Timor L'Este",
-    "Togo",
-    "Tonga",
-    "Trinidad &amp; Tobago",
-    "Tunisia",
-    "Turkey",
-    "Turkmenistan",
-    "Turks &amp; Caicos",
-    "Uganda",
-    "Ukraine",
-    "United Arab Emirates",
-    "United Kingdom",
-    "Uruguay",
-    "Uzbekistan",
-    "Venezuela",
-    "Vietnam",
-    "Virgin Islands (US)",
-    "Yemen",
-    "Zambia",
-    "Zimbabwe"
-];
+
+    {
+        "id": 0,
+        "countryName": "Afghanistan",
+        "numberOfPopulation": 0,
+        "cities": ["Kabul", "Herat", "Mazar-i-Sharif"]
+    },
+    {
+        "id": 1,
+        "countryName": "Albania",
+        "numberOfPopulation": 100,
+        "cities": ["Tirana", "Durrës", "Vlorë"]
+    },
+    {
+        "id": 2,
+        "countryName": "Algeria",
+        "numberOfPopulation": 200,
+        "cities": ["Algiers", "Oran", "Constantine"]
+    },
+    {
+        "id": 3,
+        "countryName": "Andorra",
+        "numberOfPopulation": 300,
+        "cities": ["Andorra la Vella", "Escaldes-Engordany", "Encamp"]
+    }, {
+        "id": 4,
+        "countryName": "Angola",
+        "numberOfPopulation": 400,
+        "cities": ["Luanda", "Lobito", "Huambo"]
+    }, {
+        "id": 5,
+        "countryName": "Anguilla",
+        "numberOfPopulation": 500,
+        "cities": ["The Valley", "Blowing Point Village", "Stoney Ground"]
+    }, {
+        "id": 6,
+        "countryName": "Antigua & Barbuda",
+        "numberOfPopulation": 600,
+        "cities": ["St. John's", "All Saints", "Liberta"]
+    }, {
+        "id": 7,
+        "countryName": "Argentina",
+        "numberOfPopulation": 700,
+        "cities": ["Buenos Aires", "Córdoba", "Rosario"]
+    }, {
+        "id": 8,
+        "countryName": "Armenia",
+        "numberOfPopulation": 800,
+        "cities": ["Yerevan", "Gyumri", "Vanadzor"]
+    }, {
+        "id": 9,
+        "countryName": "Aruba",
+        "numberOfPopulation": 900,
+        "cities": ["Oranjestad", "San Nicolaas", "Santa Cruz"]
+    }, {
+        "id": 10,
+        "countryName": "Australia",
+        "numberOfPopulation": 25000,
+        "cities": ["Sydney", "Melbourne", "Brisbane"]
+    }, {
+        "id": 11,
+        "countryName": "Austria",
+        "numberOfPopulation": 8900,
+        "cities": ["Vienna", "Graz", "Linz"]
+    }, {
+        "id": 12,
+        "countryName": "Azerbaijan",
+        "numberOfPopulation": 1000,
+        "cities": ["Baku", "Ganja", "Sumgait"]
+    }, {
+        "id": 13,
+        "countryName": "Bahamas",
+        "numberOfPopulation": 500,
+        "cities": ["Nassau", "Freeport", "Lucaya"]
+    }, {
+        "id": 14,
+        "countryName": "Bahrain",
+        "numberOfPopulation": 800,
+        "cities": ["Manama", "Riffa", "Muharraq"]
+    }, {
+        "id": 15,
+        "countryName": "Bangladesh",
+        "numberOfPopulation": 1600,
+        "cities": ["Dhaka", "Chittagong", "Khulna"]
+    }, {
+        "id": 16,
+        "countryName": "Barbados",
+        "numberOfPopulation": 300,
+        "cities": ["Bridgetown", "Speightstown", "Oistins"]
+    }, {
+        "id": 17,
+        "countryName": "Belarus",
+        "numberOfPopulation": 950,
+        "cities": ["Minsk", "Gomel", "Mogilev"]
+    }, {
+        "id": 18,
+        "countryName": "Belgium",
+        "numberOfPopulation": 1140,
+        "cities": ["Brussels", "Antwerp", "Ghent"]
+    }, {
+        "id": 19,
+        "countryName": "Belize",
+        "numberOfPopulation": 400,
+        "cities": ["Belmopan", "Belize City", "San Ignacio"]
+    }, {
+        "id": 20,
+        "countryName": "Benin",
+        "numberOfPopulation": 1200,
+        "cities": ["Cotonou", "Porto-Novo", "Parakou"]
+    }, {
+        "id": 21,
+        "countryName": "Bhutan",
+        "numberOfPopulation": 800,
+        "cities": ["Thimphu", "Phuntsholing", "Paro"]
+    }, {
+        "id": 22,
+        "countryName": "Bolivia",
+        "numberOfPopulation": 1100,
+        "cities": ["La Paz", "Sucre", "Santa Cruz de la Sierra"]
+    }, {
+        "id": 23,
+        "countryName": "Bosnia and Herzegovina",
+        "numberOfPopulation": 350,
+        "cities": ["Sarajevo", "Banja Luka", "Tuzla"]
+    }, {
+        "id": 24,
+        "countryName": "Botswana",
+        "numberOfPopulation": 220,
+        "cities": ["Gaborone", "Francistown", "Molepolole"]
+    }, {
+        "id": 25,
+        "countryName": "Brazil",
+        "numberOfPopulation": 21200,
+        "cities": ["Brasília", "São Paulo", "Rio de Janeiro"]
+    }, {
+        "id": 26,
+        "countryName": "Brunei",
+        "numberOfPopulation": 450,
+        "cities": ["Bandar Seri Begawan", "Kuala Belait", "Seria"]
+    }, {
+        "id": 27,
+        "countryName": "Bulgaria",
+        "numberOfPopulation": 700,
+        "cities": ["Sofia", "Plovdiv", "Varna"]
+    }, {
+        "id": 28,
+        "countryName": "Burkina Faso",
+        "numberOfPopulation": 200,
+        "cities": ["Ouagadougou", "Bobo-Dioulasso", "Koudougou"]
+    }, {
+        "id": 29,
+        "countryName": "Burundi",
+        "numberOfPopulation": 120,
+        "cities": ["Bujumbura", "Muyinga", "Ruyigi"]
+    }, {
+        "id": 30,
+        "countryName": "Cambodia",
+        "numberOfPopulation": 1600,
+        "cities": ["Phnom Penh", "Siem Reap", "Battambang"]
+    }, {
+        "id": 31,
+        "countryName": "Cameroon",
+        "numberOfPopulation": 2600,
+        "cities": ["Yaoundé", "Douala", "Bamenda"]
+    }, {
+        "id": 32,
+        "countryName": "Canada",
+        "numberOfPopulation": 38000,
+        "cities": ["Ottawa", "Toronto", "Vancouver"]
+    }, {
+        "id": 33,
+        "countryName": "Cape Verde",
+        "numberOfPopulation": 600,
+        "cities": ["Praia", "Mindelo", "Assomada"]
+    }, {
+        "id": 34,
+        "countryName": "Central African Republic",
+        "numberOfPopulation": 490,
+        "cities": ["Bangui", "Bimbo", "Berbérati"]
+    }, {
+        "id": 35,
+        "countryName": "Chad",
+        "numberOfPopulation": 1500,
+        "cities": ["N'Djamena", "Moundou", "Sarh"]
+    }, {
+        "id": 36,
+        "countryName": "Chile",
+        "numberOfPopulation": 1900,
+        "cities": ["Santiago", "Valparaíso", "Concepción"]
+    }, {
+        "id": 37,
+        "countryName": "China",
+        "numberOfPopulation": 1444216,
+        "cities": ["Beijing", "Shanghai", "Guangzhou"]
+    }, {
+        "id": 38,
+        "countryName": "Colombia",
+        "numberOfPopulation": 5100,
+        "cities": ["Bogotá", "Medellín", "Cali"]
+    }, {
+        "id": 39,
+        "countryName": "Comoros",
+        "numberOfPopulation": 800,
+        "cities": ["Moroni", "Mutsamudu", "Fomboni"]
+    }, {
+        "id": 40,
+        "countryName": "Congo (Brazzaville)",
+        "numberOfPopulation": 1800,
+        "cities": ["Brazzaville", "Pointe-Noire", "Dolisie"]
+    }, {
+        "id": 41,
+        "countryName": "Congo (Kinshasa)",
+        "numberOfPopulation": 8500,
+        "cities": ["Kinshasa", "Lubumbashi", "Mbuji-Mayi"]
+    }, {
+        "id": 42,
+        "countryName": "Costa Rica",
+        "numberOfPopulation": 510,
+        "cities": ["San Jose", "Limon", "San Francisco"]
+    }, {
+        "id": 43,
+        "countryName": "Croatia",
+        "numberOfPopulation": 400,
+        "cities": ["Zagreb", "Split", "Rijeka"]
+    }, {
+        "id": 44,
+        "countryName": "Cuba",
+        "numberOfPopulation": 1100,
+        "cities": ["Havana", "Santiago de Cuba", "Camagüey"]
+    }, {
+        "id": 45,
+        "countryName": "Cyprus",
+        "numberOfPopulation": 1200,
+        "cities": ["Nicosia", "Limassol", "Larnaca"]
+    }, {
+        "id": 46,
+        "countryName": "Czech Republic",
+        "numberOfPopulation": 1060,
+        "cities": ["Prague", "Brno", "Ostrava"]
+    }, {
+        "id": 47,
+        "countryName": "Denmark",
+        "numberOfPopulation": 580,
+        "cities": ["Copenhagen", "Aarhus", "Odense"]
+    }, {
+        "id": 48,
+        "countryName": "Djibouti",
+        "numberOfPopulation": 980,
+        "cities": ["Djibouti City", "Ali Sabieh", "Tadjoura"]
+    }, {
+        "id": 49,
+        "countryName": "Dominica",
+        "numberOfPopulation": 70,
+        "cities": ["Roseau", "Portsmouth", "Marigot"]
+    }, {
+        "id": 50,
+        "countryName": "Dominican Republic",
+        "numberOfPopulation": 1100,
+        "cities": ["Santo Domingo", "Santiago", "La Romana"]
+    }, {
+        "id": 51,
+        "countryName": "Ecuador",
+        "numberOfPopulation": 1750,
+        "cities": ["Quito", "Guayaquil", "Cuenca"]
+    }, {
+        "id": 52,
+        "countryName": "Egypt",
+        "numberOfPopulation": 10426,
+        "cities": ["Cairo", "Alexandria", "Giza"]
+    }, {
+        "id": 53,
+        "countryName": "El Salvador",
+        "numberOfPopulation": 680,
+        "cities": ["San Salvador", "Santa Ana", "San Miguel"]
+    }, {
+        "id": 54,
+        "countryName": "Equatorial Guinea",
+        "numberOfPopulation": 1400,
+        "cities": ["Malabo", "Bata", "Ebebiyin"]
+    }, {
+        "id": 55,
+        "countryName": "Eritrea",
+        "numberOfPopulation": 350,
+        "cities": ["Asmara", "Keren", "Massawa"]
+    }, {
+        "id": 56,
+        "countryName": "Estonia",
+        "numberOfPopulation": 1300,
+        "cities": ["Tallinn", "Tartu", "Narva"]
+    }, {
+        "id": 57,
+        "countryName": "Eswatini",
+        "numberOfPopulation": 1160,
+        "cities": ["Mbabane", "Manzini", "Big Bend"]
+    }, {
+        "id": 58,
+        "countryName": "Ethiopia",
+        "numberOfPopulation": 12643,
+        "cities": ["Addis Ababa", "Dire Dawa", "Mekelle"]
+    }, {
+        "id": 59,
+        "countryName": "Fiji",
+        "numberOfPopulation": 890,
+        "cities": ["Suva", "Lautoka", "Nadi"]
+    }, {
+        "id": 60,
+        "countryName": "Finland",
+        "numberOfPopulation": 5540,
+        "cities": ["Helsinki", "Espoo", "Tampere"]
+    }, {
+        "id": 61,
+        "countryName": "France",
+        "numberOfPopulation": 67120,
+        "cities": ["Paris", "Marseille", "Lyon"]
+    }, {
+        "id": 62,
+        "countryName": "Gabon",
+        "numberOfPopulation": 2225,
+        "cities": ["Libreville", "Port-Gentil", "Franceville"]
+    }, {
+        "id": 63,
+        "countryName": "Gambia",
+        "numberOfPopulation": 240,
+        "cities": ["Banjul", "Serekunda", "Brikama"]
+    }, {
+        "id": 64,
+        "countryName": "Georgia",
+        "numberOfPopulation": 3989,
+        "cities": ["Tbilisi", "Kutaisi", "Batumi"]
+    }, {
+        "id": 65,
+        "countryName": "Germany",
+        "numberOfPopulation": 83129,
+        "cities": ["Berlin", "Hamburg", "Munich"]
+    }, {
+        "id": 66,
+        "countryName": "Ghana",
+        "numberOfPopulation": 31072,
+        "cities": ["Accra", "Kumasi", "Tamale"]
+    }, {
+        "id": 67,
+        "countryName": "Greece",
+        "numberOfPopulation": 10741,
+        "cities": ["Athens", "Thessaloniki", "Patras"]
+    }, {
+        "id": 68,
+        "countryName": "Grenada",
+        "numberOfPopulation": 110,
+        "cities": ["St. George's", "Gouyave", "Grenville"]
+    }, {
+        "id": 69,
+        "countryName": "Guatemala",
+        "numberOfPopulation": 18066,
+        "cities": ["Guatemala City", "Mixco", "Villa Nueva"]
+    }, {
+        "id": 70,
+        "countryName": "Guinea",
+        "numberOfPopulation": 12717,
+        "cities": ["Conakry", "Nzérékoré", "Kankan"]
+    }, {
+        "id": 71,
+        "countryName": "Guinea-Bissau",
+        "numberOfPopulation": 1968,
+        "cities": ["Bissau", "Bafatá", "Gabú"]
+    }, {
+        "id": 72,
+        "countryName": "Guyana",
+        "numberOfPopulation": 788,
+        "cities": ["Georgetown", "Linden", "New Amsterdam"]
+    }, {
+        "id": 73,
+        "countryName": "Haiti",
+        "numberOfPopulation": 11402,
+        "cities": ["Port-au-Prince", "Carrefour", "Delmas"]
+    }, {
+        "id": 74,
+        "countryName": "Honduras",
+        "numberOfPopulation": 10027,
+        "cities": ["Tegucigalpa", "San Pedro Sula", "Choloma"]
+    }, {
+        "id": 75,
+        "countryName": "Hungary",
+        "numberOfPopulation": 9660,
+        "cities": ["Budapest", "Debrecen", "Miskolc"]
+    }, {
+        "id": 76,
+        "countryName": "Iceland",
+        "numberOfPopulation": 339,
+        "cities": ["Reykjavik", "Kopavogur", "Hafnarfjordur"]
+    }, {
+        "id": 77,
+        "countryName": "India",
+        "numberOfPopulation": 1380000,
+        "cities": ["New Delhi", "Mumbai", "Bangalore"]
+    }, {
+        "id": 78,
+        "countryName": "Indonesia",
+        "numberOfPopulation": 273523,
+        "cities": ["Jakarta", "Surabaya", "Bandung"]
+    }, {
+        "id": 79,
+        "countryName": "Iran",
+        "numberOfPopulation": 82802,
+        "cities": ["Tehran", "Mashhad", "Isfahan"]
+    }, {
+        "id": 80,
+        "countryName": "Iraq",
+        "numberOfPopulation": 40377,
+        "cities": ["Baghdad", "Basra", "Mosul"]
+    }, {
+        "id": 81,
+        "countryName": "Ireland",
+        "numberOfPopulation": 4965,
+        "cities": ["Dublin", "Cork", "Limerick"]
+    }, {
+        "id": 82,
+        "countryName": "Israel",
+        "numberOfPopulation": 9053,
+        "cities": ["Jerusalem", "Tel Aviv", "Haifa"]
+    }, {
+        "id": 83,
+        "countryName": "Italy",
+        "numberOfPopulation": 60360,
+        "cities": ["Rome", "Milan", "Naples"]
+    }, {
+        "id": 84,
+        "countryName": "Jamaica",
+        "numberOfPopulation": 2950,
+        "cities": ["Kingston", "Spanish Town", "Montego Bay"]
+    }, {
+        "id": 85,
+        "countryName": "Japan",
+        "numberOfPopulation": 126476,
+        "cities": ["Tokyo", "Yokohama", "Osaka"]
+    }, {
+        "id": 86,
+        "countryName": "Jordan",
+        "numberOfPopulation": 10816,
+        "cities": ["Amman", "Zarqa", "Irbid"]
+    }, {
+        "id": 87,
+        "countryName": "Kazakhstan",
+        "numberOfPopulation": 18776,
+        "cities": ["Nur-Sultan", "Almaty", "Karaganda"]
+    }, {
+        "id": 88,
+        "countryName": "Kenya",
+        "numberOfPopulation": 53771,
+        "cities": ["Nairobi", "Mombasa", "Kisumu"]
+    }, {
+        "id": 89,
+        "countryName": "Kiribati",
+        "numberOfPopulation": 123,
+        "cities": ["Tarawa", "Betio", "Bairiki"]
+    }, {
+        "id": 90,
+        "countryName": "Kuwait",
+        "numberOfPopulation": 4327,
+        "cities": ["Kuwait City", "Al Ahmadi", "Hawalli"]
+    }, {
+        "id": 91,
+        "countryName": "Kyrgyzstan",
+        "numberOfPopulation": 6558,
+        "cities": ["Bishkek", "Osh", "Jalal-Abad"]
+    }, {
+        "id": 92,
+        "countryName": "Laos",
+        "numberOfPopulation": 7224,
+        "cities": ["Vientiane", "Pakse", "Savannakhet"]
+    }, {
+        "id": 93,
+        "countryName": "Latvia",
+        "numberOfPopulation": 1907,
+        "cities": ["Riga", "Daugavpils", "Liepaja"]
+    }, {
+        "id": 94,
+        "countryName": "Lebanon",
+        "numberOfPopulation": 6835,
+        "cities": ["Beirut", "Tripoli", "Sidon"]
+    }, {
+        "id": 95,
+        "countryName": "Lesotho",
+        "numberOfPopulation": 2159,
+        "cities": ["Maseru", "Teyateyaneng", "Mafeteng"]
+    }, {
+        "id": 96,
+        "countryName": "Liberia",
+        "numberOfPopulation": 4937,
+        "cities": ["Monrovia", "Ganta", "Gbarnga"]
+    }, {
+        "id": 97,
+        "countryName": "Libya",
+        "numberOfPopulation": 6871,
+        "cities": ["Tripoli", "Benghazi", "Misrata"]
+    }, {
+        "id": 98,
+        "countryName": "Liechtenstein",
+        "numberOfPopulation": 38,
+        "cities": ["Vaduz", "Schaan", "Triesen"]
+    }, {
+        "id": 99,
+        "countryName": "Lithuania",
+        "numberOfPopulation": 2794,
+        "cities": ["Vilnius", "Kaunas", "Klaipeda"]
+    }, {
+        "id": 100,
+        "countryName": "Luxembourg",
+        "numberOfPopulation": 633,
+        "cities": ["Luxembourg City", "Esch-sur-Alzette", "Differdange"]
+    }
+
+]
