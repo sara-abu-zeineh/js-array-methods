7cb8d88d00e79d1514f4e8d307432b53d56c8c90
Sarah Abu Zeneh <sarah.abuzeneh@gmail.com>
2023-10-12 10:53:15 +0300

    Create the Country Class

---
+++ Country.mjs
@@ -0,0 +1,8 @@
+export class Country {
+    constructor(id, countryName, numberOfPopulation, cities) {
+        this.id = id;
+        this.countryName = countryName;
+        this.numberOfPopulation = numberOfPopulation;
+        this.cities = [cities];
+    }
+}
